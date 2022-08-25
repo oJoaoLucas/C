d187df59de69011c681a7cb2dfe89651f5f793c9
João Lucas <52823911+oJoaoLucas@users.noreply.github.com>
2022-08-25 20:33:03 -0300

    Add files via upload

---
+++ multiplicacao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    float x, total;
+
+    num = 2;
+    x = 135.4;
+
+    total = num * x;
+
+    printf("Multiplicacao = %.2f\n", total);
+
+    return 0;
+}
